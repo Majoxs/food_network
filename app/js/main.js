2b26b4e0d97e28a24f229eb5d08376642c2f8e8f
Majoxs <majoxs@gmail.com>
2021-07-11 13:05:18 +0500

    added adaptive menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,42 @@
+// Menu
+
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+   iconMenu.addEventListener('click', function (e) {
+      document.body.classList.toggle('_lock');
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+   });
+}
+
+
+
+//Smooth scrolling
+
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (let anchor of anchors) {
+   anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      const blockId = anchor.getAttribute('href').substr(1);
+
+      if (iconMenu.classList.contains('_active')) {
+         document.body.classList.remove('_lock');
+         iconMenu.classList.remove('_active')
+         menuBody.classList.remove('_active')
+      }
+
+      document.getElementById(blockId).scrollIntoView({
+         behavior: 'smooth',
+         block: 'start',
+      })
+
+
+   })
+}
+
 
 //Star rating
 
